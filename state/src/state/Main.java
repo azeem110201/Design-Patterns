034db19efe2b3e69ef043fb9c245a0ef2681b9c5
azeem110201 <mohd.abazeem@gmail.com>
2022-11-10 12:03:53 +0530

    state design-patterns

---
+++ state/src/state/Main.java
@@ -0,0 +1,19 @@
+package state;
+
+public class Main {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        simulatePhoneClicks(phone);
+    }
+
+    private static void simulatePhoneClicks(Phone phone) {
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickHome());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickPower());
+        System.out.println(phone.clickHome());
+    }
+}
