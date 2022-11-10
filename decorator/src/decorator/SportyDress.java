1c5b17839aab76c545267163169613d643f5d365
azeem110201 <mohd.abazeem@gmail.com>
2022-11-10 11:51:52 +0530

    decorator design pattern

---
+++ decorator/src/decorator/SportyDress.java
@@ -0,0 +1,13 @@
+package decorator;
+
+public class SportyDress extends DressDecorator{
+    public SportyDress(Dress c) {
+        super(c);
+    }
+
+    @Override
+    public void assemble() {
+        super.assemble();
+        System.out.println("Adding Sporty Dress Features");
+    }
+}
